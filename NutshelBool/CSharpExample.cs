5673cc2ed3f1ee3ae48d289990a219ec8a20880b
Alireza Rezaee <emailer1.ir@gmail.com>
2023-06-14 12:31:34 +0330

    c Sharp Nutshel Until Page 15

---
+++ NutshelBool/CSharpExample.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutshelBool
+{
+    public class CSharpExample: IExampleLogger
+    {
+        public void NondestructiveMutationForAnonymousypes()
+        {
+
+            //در سی شارپ 10 اضافه شده است
+            //با استفاده از این کلمه کلیدی می توان مقدار جدید را به شی که قبلا ساخته شده است اضافه کنیم
+            var a1 = new { a = 1, b = 2, c = 3, d = 4, e = 5 };
+
+            var a2 = a1 with { e = 10 };
+
+
+            //برای اینکه برای کلاس از این روش استفاده کنیم باید از 
+            //record
+            //در زمان ایجاد کلاس اصلی استفاده کنیم
+            var person = new PersonRecordNutshel { Id = 1, Name = "Alireza" };
+
+            var newPerson = person with { Age = 10 };
+
+
+            Console.WriteLine(a2);
+        }
+
+       
+        public void NewDeconstructionSyntax()
+        {
+            var point = (3, 4);
+
+            double x = 0;
+
+            (x,double y) = point;
+
+            string first = $"{x}";
+
+            string secend = $"{y}";
+
+            Console.WriteLine(first +" "+ secend);
+        }
+
+
+        public void PatternMatchingImprovements()
+        {
+            string GetStatus(int sts) => sts switch
+            {
+                > 15 => Status.High.ToString(),
+                < 15 => Status.Normall.ToString(),
+                > 9 => Status.low.ToString(),
+                
+            };
+            
+        }
+
+
+        
+    }
+
+
+    public record class PersonRecordNutshel
+    {
+
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public enum Status
+    {
+        low,
+        Normall,
+        High
+    }
+
+
+    /// <summary>
+    /// با اینترفیس که بدنه داریم می توانیم مواردی که مشترک اسست یا هر چیزی استفاده کنیم
+    /// ولی برای استفاده از این ویژگی 
+    /// این باید به وسیله یک کلاس پیاده سازی بشه 
+    /// </summary>
+    public interface IExampleLogger
+    {
+        
+        void ExampleLog(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+}
--- NutshelBool/Program.cs
+++ NutshelBool/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using NutshelBool;
+
+CSharpExample examp = new CSharpExample();
+
+//examp.NondestructiveMutationForAnonymousypes();
+
+examp.NewDeconstructionSyntax();
+
+var logger = (IExampleLogger)examp;
+logger.ExampleLog("Test");
+Console.ReadLine();
