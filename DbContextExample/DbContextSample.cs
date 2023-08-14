c70afc59c4bf0ac2ac32fab918c8c4fd89510b0f
Alireza Rezaee <emailer1.ir@gmail.com>
2023-08-14 20:58:57 +0330

    Nikamooz EfCore S24 Setting The EntityState

--- DbContextExample/DbContextSample.cs
+++ DbContextExample/DbContextSample.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace DbContextExample
 {
@@ -43,5 +44,43 @@ namespace DbContextExample
             var contextId1 = db1.ContextId;
             var contextId2 = db2.ContextId;
         }
+    }
+
+
+    public class SettingTheEntityState
+    {
+        private readonly DbContextSample _db;
+
+        public SettingTheEntityState(DbContextSample db)
+        {
+            _db = db;
+        }
+
+        //در اینجا میخواهیم وضعیت نشست را تغییر بدهیم
+
+        public void SetState()
+        {
+            //در اینجا یک مدل جدید پرسن می سازیم که آیدی این رکورد در دیتابیس وجود دارد
+            //آیدی 10 در دیتابیس وجود دارد
+            Person person1 = new Person { Id = 10, FirstName = "Alireza", LastName = "Rezaee" };
+
+            //در اینجا وضعیت مدل ما
+            //Detached
+            //می باشد چون چنج ترکر خبری از این مدل ندارد
+            Console.WriteLine(_db.Entry(person1).State);
+
+            //در اینجا وضعیت به 
+            //modified 
+            //تغییر می کند
+            _db.Entry(person1).State = EntityState.Modified;
+
+            //وقتی ذخیره را انجام بدیم کل مدل را در دیتابیس آپدیت می کند
+            //چون مدل وجود داره میره و آپدیت می کنه
+
+
+            //با دستور زیر پروپرتی مذکور از موارد قابل تغییر خارج میشه
+            _db.Entry(person1).Property(e => e.FirstName).IsModified = false;
+        }
+
     }
 }
