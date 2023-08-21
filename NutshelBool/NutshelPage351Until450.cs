0c633c21070aab81981e2c9a2798f590bb76ab0a
Alireza Rezaee <emailer1.ir@gmail.com>
2023-08-21 21:08:21 +0330

    C# Nutshel Page 352 Until ICollection<T> and ICollection

---
+++ NutshelBool/NutshelPage351Until450.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutshelBooK
+{
+    internal class NutshelPage351Until450
+    {
+    }
+
+
+    #region The ICollection And IList Interface
+    //شمارشگرهای 
+    //IEnumerable And IEnumerator
+    //فقط شمارشگر رو به جلو هستند و امکانات زیر را ارائه نمی دهند
+    //امکان اندازه گیری تعداد کالکشن
+    //امکان کار  و دسترسی بر روی ایندکس معین
+    //امکان جستجو در کالکشن
+
+    //بهترین کالکشن های در نظر گرفته شده برای کار بر روی ایندکس و ...
+    //ICollection and IList and IDictionary
+    #endregion
+}
