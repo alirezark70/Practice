30bf99628f9fe64fedd01034dc7ff37b9980178d
Alireza Rezaee <emailer1.ir@gmail.com>
2023-08-22 11:48:16 +0330

    C# Nutshel Page 359 Until Enumeration

--- NutshelBool/NutshelPage351Until450.cs
+++ NutshelBool/NutshelPage351Until450.cs
@@ -21,5 +21,44 @@ namespace NutshelBooK
 
     //بهترین کالکشن های در نظر گرفته شده برای کار بر روی ایندکس و ...
     //ICollection and IList and IDictionary
+
+
+    //IList<T>
+    //بهتر و کاربردی تر از 
+    //ICollection
+    //می باشد
+
+
+    #endregion
+
+
+
+    #region IList<T> and IList
+    public class IListTAndIListClass
+    {
+
+    }
+
+    #endregion
+
+
+    #region Array Class
+    //آرایه یک رفرنس تایپ می باشد که از قوانین رفرنس تایپ ها هم پیروی می کند
+    //یعنی دو مقدار شبیه هم باهم برابر نیستند چن رفرنس های مرجع متفاوتی دارند
+
+    public class ArrayClass
+    {
+        void TestArray()
+        {
+            var arrayList = Array.CreateInstance(typeof(string), 3);
+
+            arrayList.SetValue("Alireza", 0);
+
+
+            string[] s1 = (string[])arrayList;
+
+        }
+    }
+
     #endregion
 }
