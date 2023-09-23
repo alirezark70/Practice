682f503b0581b944641e28c4c6aef128c643793e
Alireza Rezaee <emailer1.ir@gmail.com>
2023-09-23 04:40:45 +0330

    Single Responsibility Principle

---
+++ Solid/SingleResponibilityPrinciple.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid
+{
+    //مفاهیم
+    /// <summary>
+    /// low coupling
+    /// به معنای درهم تپیدگی
+    /// در مفهوم توسعه نرم افزار از درهم تپیدگی باید جلوگیری کنیم
+    /// 
+    /// 
+    /// high cohesion
+    /// کوهیژن یعنی پیوستگی که باید بالا ببریم
+    /// 
+    /// </summary>
+    /// 
+    
+    //https://learn.microsoft.com/en-us/archive/msdn-magazine/2014/may/csharp-best-practices-dangers-of-violating-solid-principles-in-csharp
+    public class SingleResponibilityPrinciple
+    {
+        ///تعریف اصلی 
+        ///The Single Responsibility Principle is often defined as: An object should only have one reason to change
+        ///یک شی فقط یک دلیل باید برای تغییر داشته باشد
+        ///هرچه فایل یا کلاس طولانی تر باشد دستیابی به این امر دشوار تر می شود
+        ///برای رسیدن به این هدف به صورت کلی نباید دو آبجکت یا کلاس را برای هدفی با هم ترکیب کرد
+
+        public void Execute()
+        {
+             
+        }
+
+
+    }
+}
