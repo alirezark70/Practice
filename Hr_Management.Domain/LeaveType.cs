85bb7232dfd2ba371647f2393d095cf02b78509e
Alireza Rezaee <emailer1.ir@gmail.com>
2024-03-26 20:03:30 +0330

    Create Solution For Learn Hr_Management

---
+++ Hr_Management.Domain/LeaveType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hr_Management.Domain
+{
+    public class LeaveType
+    {
+        public string Name { get; set; }
+        public int DefaultDay { get; set; }
+    }
+}
