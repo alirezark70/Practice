1f52ca32bcf88bc7cb6bb7f916f8b7bd6f2790f3
Alireza Rezaee <emailer1.ir@gmail.com>
2024-12-04 02:04:18 +0330

    Add

---
+++ DotNetCoreSample/RoutingSession31/UseRoutingSampleClass.cs
@@ -0,0 +1,20 @@
+﻿namespace DotNetCoreSample.RoutingSession31
+{
+    public static class UseRoutingSampleClass
+    {
+        
+        public static void UseRunSample(this WebApplication application)
+        {
+            application.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Is Ok");
+            });
+        }
+
+        public static void UseLearningRouting(this WebApplication application)
+        {
+
+        }
+    }
+
+}
