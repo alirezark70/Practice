f982795eb4f0798b766b0415724374e8a6526686
Alireza Rezaee <emailer1.ir@gmail.com>
2025-01-12 11:08:54 +0330

    f

---
+++ BasicSSharp/IndexerSample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSSharp
+{
+    public class IndexerSample
+    {
+        ImplementationIndexer indexer=new ImplementationIndexer();
+
+        public void Proseccing()
+        {
+            indexer[109];
+        }
+    }
+
+    public class ImplementationIndexer
+    {
+        public string this[int input]
+        {
+            get { return this[input]; }
+
+            set { this[input] ="sample" +value; }
+        }
+    }
+}
---
+++ SsharpBasic/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using SsharpBasic;
+
+IndexerSample sample = new IndexerSample();
+
+var result = sample.Proseccing();
+
+var alireza = result[70];
+Console.WriteLine("Hello, World!");
