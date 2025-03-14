d64a8b0862269a378bcd044cf38655c31ea8172e
Alireza Rezaee <emailer1.ir@gmail.com>
2025-03-14 15:51:45 +0330

    implement rabbitMq Sender using pattern

--- RabbitMQ.HelloWord.Sender/Program.cs
+++ RabbitMQ.HelloWord.Sender/Program.cs
@@ -1,63 +1,81 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.HelloWord.Sender.Senders;
 using System.Text;
 
 
-    // شیء ConnectionFactory جهت تعیین پارامترهای اتصال به سرور RabbitMQ (لوکال یا ریموت)  
-    var connectionFactory = new ConnectionFactory
-    {
-        HostName = "localhost",   // نشانی سرور؛ اگر RabbitMQ روی سیستم محلی باشد "localhost"  
-        Port = 5672,             // پورت پیش‌فرض برای اتصال  
-        UserName = "guest",       // نام کاربری پیش‌فرض؛  
-        Password = "guest",       // رمز عبور پیش‌فرض؛  
+//استفاده از پترن های
+//DI
+//Strategy Pattern
+//Connection Pool
 
-        // فعال کردن بازیابی خودکار در صورت قطع ارتباط با سرور  
-        AutomaticRecoveryEnabled = true,
+var config = new RabbitMQConfig(hostName: "localhost",5672,"guest", "guest",true, TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(30), "my_test_queue");
+var connectionPool=new RabbitMQConnectionPool(config,10);
+ISender send = new DefaultSend(connectionPool);
 
-        // هنگامی که اتصال قطع شود، بعد از 10 ثانیه تلاش می‌کند دوباره وصل شود  
-        NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
+string massage = "I am Alireza ! Hello";
 
-        // بازه زمانی ضربان (Heartbeat) برای بررسی سلامت ارتباط  
-        RequestedHeartbeat = TimeSpan.FromSeconds(30)
-    };
+var sender = new SendClass(send, config, massage);
 
-    // ایجاد اتصال (Connection) به صورت Async  
-    // using var باعث می‌شود در انتهای بلاک، اتصال Dispose شود  
-    using var connection = await connectionFactory.CreateConnectionAsync();
+await sender.Send();
 
-    // ایجاد کانال (Channel) به صورت Async  
-    // همه عملیات ارسال و دریافت پیام از طریق Channel انجام می‌شود  
-    using var channel = await connection.CreateChannelAsync();
+//حالت ساده پیاده سازی
 
-    // نام صف یا Queue که پیام‌ها قرار است به آن ارسال شوند  
-    string queueName = "my_test_queue";
+// شیء ConnectionFactory جهت تعیین پارامترهای اتصال به سرور RabbitMQ (لوکال یا ریموت)  
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost",   // نشانی سرور؛ اگر RabbitMQ روی سیستم محلی باشد "localhost"  
+    Port = 5672,             // پورت پیش‌فرض برای اتصال  
+    UserName = "guest",       // نام کاربری پیش‌فرض؛  
+    Password = "guest",       // رمز عبور پیش‌فرض؛  
 
-    // تعریف صف با پارامترهای مورد نظر:  
-    // durable = false => صف موقت بوده و پس از ری‌استارت سرور RabbitMQ پاک می‌شود  
-    // exclusive = true => صف فقط برای همین اتصال قابل استفاده خواهد بود و با بسته شدن اتصال حذف می‌شود  
-    // autoDelete = false => صف با قطع ارتباط مصرف‌کنندگان به صورت خودکار حذف نخواهد شد  
-    await channel.QueueDeclareAsync(
-        queue: queueName,
-        durable: false,
-        exclusive: true,
-        autoDelete: false
-    );
+    // فعال کردن بازیابی خودکار در صورت قطع ارتباط با سرور  
+    AutomaticRecoveryEnabled = true,
 
-    // محتوای متنی پیام  
-    string message = "Hello from .NET 8!";
+    // هنگامی که اتصال قطع شود، بعد از 10 ثانیه تلاش می‌کند دوباره وصل شود  
+    NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
 
-    // تبدیل متن به آرایه بایت (RabbitMQ پیام‌ها را به صورت باینری مدیریت می‌کند)  
-    var body = Encoding.UTF8.GetBytes(message);
+    // بازه زمانی ضربان (Heartbeat) برای بررسی سلامت ارتباط  
+    RequestedHeartbeat = TimeSpan.FromSeconds(30)
+};
 
-    // ارسال پیام به صورت Async  
-    // exchange = "" => استفاده از Exchange پیش‌فرض  
-    // routingKey = نام صف => در صورت استفاده از Exchange پیش‌فرض، باید نام صف را قرار دهیم  
-    await channel.BasicPublishAsync(
-        exchange: "",
-        routingKey: queueName,
-        body: body
-    );
+// ایجاد اتصال (Connection) به صورت Async  
+// using var باعث می‌شود در انتهای بلاک، اتصال Dispose شود  
+using var connection = await connectionFactory.CreateConnectionAsync();
 
-    // نمایش نتیجه در خروجی کنسول  
-    Console.WriteLine($"پیام ارسال شد: {message}");
+// ایجاد کانال (Channel) به صورت Async  
+// همه عملیات ارسال و دریافت پیام از طریق Channel انجام می‌شود  
+using var channel = await connection.CreateChannelAsync();
+
+// نام صف یا Queue که پیام‌ها قرار است به آن ارسال شوند  
+string queueName = "my_test_queue";
+
+// تعریف صف با پارامترهای مورد نظر:  
+// durable = false => صف موقت بوده و پس از ری‌استارت سرور RabbitMQ پاک می‌شود  
+// exclusive = true => صف فقط برای همین اتصال قابل استفاده خواهد بود و با بسته شدن اتصال حذف می‌شود  
+// autoDelete = false => صف با قطع ارتباط مصرف‌کنندگان به صورت خودکار حذف نخواهد شد  
+await channel.QueueDeclareAsync(
+    queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false
+);
+
+// محتوای متنی پیام  
+string message = "Hello from .NET 8!";
+
+// تبدیل متن به آرایه بایت (RabbitMQ پیام‌ها را به صورت باینری مدیریت می‌کند)  
+var body = Encoding.UTF8.GetBytes(message);
+
+// ارسال پیام به صورت Async  
+// exchange = "" => استفاده از Exchange پیش‌فرض  
+// routingKey = نام صف => در صورت استفاده از Exchange پیش‌فرض، باید نام صف را قرار دهیم  
+await channel.BasicPublishAsync(
+    exchange: "",
+    routingKey: queueName,
+    body: body
+);
+
+نمایش نتیجه در خروجی کنسول  
+    
     Console.WriteLine("برای خروج کلیدی فشار دهید...");
     Console.ReadKey();
---
+++ RabbitMQ.HelloWord.Sender/Senders/DefaultSender.cs
@@ -0,0 +1,212 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.HelloWord.Sender.Senders
+{
+    public interface ISender:IDisposable
+    {
+        Task SendMessageAsync(RabbitMQConfig config, byte[] message);
+    }
+
+    public class SendClass 
+    {
+        private readonly ISender _sender;
+        private readonly RabbitMQConfig _config;
+        private readonly string _message;
+
+        public SendClass(ISender sender, RabbitMQConfig config, string message)
+        {
+            this._sender = sender;
+            _config = config;
+            _message = message;
+        }
+
+
+        public async Task Send()
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(_message);
+
+            await _sender.SendMessageAsync(_config, messageBytes);
+        }
+    }
+
+    public class DefaultSend : ISender
+    {
+        private readonly RabbitMQConnectionPool _rabbitMQConnectionPool;
+        public DefaultSend(RabbitMQConnectionPool rabbitMQConnectionPool)
+        {
+            _rabbitMQConnectionPool = rabbitMQConnectionPool;
+        }
+
+        public async Task SendMessageAsync(RabbitMQConfig config, byte[] message)
+        {
+            var connectionPool=new RabbitMQConnectionPool(config,10);
+
+            // ایجاد کانال (Channel) به صورت Async  
+            // همه عملیات ارسال و دریافت پیام از طریق Channel انجام می‌شود  
+            using var connection = await connectionPool.GetConnectionAsync();
+
+            using var channel= await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(
+                queue: config.QueueName,
+                durable: config.Durable,
+                exclusive: config.Exclusive,
+                autoDelete: config.AutoDelete
+            );
+
+            // ارسال پیام به صورت Async  
+            // exchange = "" => استفاده از Exchange پیش‌فرض  
+            // routingKey = نام صف => در صورت استفاده از Exchange پیش‌فرض، باید نام صف را قرار دهیم  
+            await channel.BasicPublishAsync(
+                exchange: "",
+                routingKey: config.QueueName,
+                body: message
+            );
+        }
+
+        public void Dispose()
+        {
+            _rabbitMQConnectionPool?.Dispose();
+        }
+    }
+
+
+
+
+    
+
+    public class RabbitMQConfig
+    {
+       
+
+        public RabbitMQConfig(string hostName, int port, string userName, string password, bool automaticRecoveryEnabled,
+            TimeSpan networkRecoveryInterval, TimeSpan requestedHeartbeat, string queueName, bool durable = false, 
+            bool exclusive = false, bool autoDelete = false)
+        {
+            HostName = hostName;
+            Port = port;
+            UserName = userName;
+            Password = password;
+            AutomaticRecoveryEnabled = automaticRecoveryEnabled;
+            NetworkRecoveryInterval = networkRecoveryInterval;
+            RequestedHeartbeat = requestedHeartbeat;
+            QueueName = queueName;
+            Durable = durable;
+            Exclusive = exclusive;
+            AutoDelete = autoDelete;
+
+           
+        }
+
+        public string HostName { get; set; }
+
+        public int Port { get; set; }
+        
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool AutomaticRecoveryEnabled { get; set; }
+
+        public TimeSpan NetworkRecoveryInterval { get; set; }
+
+        public TimeSpan RequestedHeartbeat { get; set; }
+
+        public string QueueName { get; set; } 
+
+        public bool Durable { get; set; } = false;
+        public bool Exclusive { get; set; } = false;
+        public bool AutoDelete { get; set; } = false;
+
+    }
+
+
+    public class RabbitMQConnectionPool : IDisposable
+    {
+        private readonly ConcurrentBag<IConnection> _connections;
+        private readonly ConnectionFactory _connectionFactory;
+
+        private readonly int _maxConnections;
+        private bool _disposed;
+
+        public RabbitMQConnectionPool(RabbitMQConfig config,int maxConnections)
+        {
+            _connections= new ConcurrentBag<IConnection>();
+
+            _connectionFactory = new ConnectionFactory
+            {
+                HostName = config.HostName,   // نشانی سرور؛ اگر RabbitMQ روی سیستم محلی باشد "localhost"  
+                Port = config.Port,             // پورت پیش‌فرض برای اتصال  
+                UserName = config.UserName,       // نام کاربری پیش‌فرض؛  
+                Password = config.Password,       // رمز عبور پیش‌فرض؛  
+
+                // فعال کردن بازیابی خودکار در صورت قطع ارتباط با سرور  
+                AutomaticRecoveryEnabled = config.AutomaticRecoveryEnabled,
+
+                // هنگامی که اتصال قطع شود، بعد از 10 ثانیه تلاش می‌کند دوباره وصل شود  
+                NetworkRecoveryInterval = config.NetworkRecoveryInterval,
+
+                // بازه زمانی ضربان (Heartbeat) برای بررسی سلامت ارتباط  
+                RequestedHeartbeat = config.RequestedHeartbeat,
+            };
+            _maxConnections = maxConnections;
+
+            // اجرای غیر همزمان در یک Task  
+            InitializeConnectionsAsync().Wait(); // انتظار برای اتمام عملیات  
+        }
+
+        private async Task InitializeConnectionsAsync()
+        {
+            var tasks = new List<Task>(); // ایجاد یک لیست برای نگهداری Task ها  
+
+            // ایجاد اتصالات به صورت غیر همزمان  
+            for (int i = 0; i < _maxConnections; i++)
+            {
+                tasks.Add(CreateConnection()); // اضافه کردن Task به لیست  
+            }
+
+            await Task.WhenAll(tasks); // منتظر اتمام تمام Task ها  
+        }
+
+        public async Task<IConnection> GetConnectionAsync()
+        {
+            if (_connections.TryTake(out var connection))
+            {
+                return connection; // اگر اتصال موجود باشد، آن را برمی‌گرداند  
+            }
+            else
+            {
+                return await CreateConnection(); // در صورت عدم وجود، اتصال جدید ایجاد می‌شود  
+            }
+        }
+
+        private async Task<IConnection> CreateConnection()
+        {
+            var connection =await _connectionFactory.CreateConnectionAsync();
+            _connections.Add(connection);
+
+            return connection;
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                foreach (var connection in _connections)
+                {
+                    connection.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+    }
+
+}
+
