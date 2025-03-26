0f6b4d6f9d3d77f8b93383fef663b3d80bbd5931
Alireza Rezaee <emailer1.ir@gmail.com>
2025-03-27 02:32:01 +0330

    Practices for decorator design pattern

---
+++ StructuralPatternSample/DecoratorDesignPatternLoginSample.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace StructuralPatternSample
+{
+    internal class DecoratorDesignPatternLoginSample
+    {
+    }
+
+
+    public interface ILogin
+    {
+        void Login();
+    }
+
+    public class Login : ILogin
+    {
+        void ILogin.Login()
+        {
+            Console.WriteLine("Login Success");
+        }
+    }
+
+    public class LoginDecorator : ILogin
+    {
+        private readonly ILogin _login;
+
+        public LoginDecorator(ILogin login)
+        {
+            _login = login;
+        }
+
+        public virtual void Login()
+        {
+            _login.Login();
+        }
+    }
+
+
+    public class SmsLoginDecorator : LoginDecorator
+    {
+        public SmsLoginDecorator(ILogin login) : base(login)
+        {
+        }
+
+        public override void Login()
+        {
+
+            base.Login();
+
+            Console.WriteLine("Send Sms");
+        }
+    }
+}
--- StructuralPatternSample/Program.cs
+++ StructuralPatternSample/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+
+using StructuralPatternSample;
+
+
+var login=new SmsLoginDecorator(new Login());
+
+login.Login();
+
+Console.ReadLine();
