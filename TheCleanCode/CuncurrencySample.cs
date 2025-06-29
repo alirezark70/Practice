07d061c16d4db1ec6b698171e6b3498d2ba728db
Alireza Rezaee <emailer1.ir@gmail.com>
2025-06-30 00:22:37 +0330

    fix compile error

--- TheCleanCode/CuncurrencySample.cs
+++ TheCleanCode/CuncurrencySample.cs
@@ -9,65 +9,66 @@ namespace TheCleanCode
 {
     //share no , Copy Data For thread saftly
     public class CuncurrencySample
-    {
-        public void CalculateNumber()
-        {
-            var input =IEnumerable<long>.(1,1000000).ToList();
+    { 
+    //{
+    //    public void CalculateNumber()
+    //    {
+    //        var input =IEnumerable<long>.(1,1000000).ToList();
 
-            Stopwatch sw = Stopwatch.StartNew();
+    //        Stopwatch sw = Stopwatch.StartNew();
 
-            sw.Start();
-            var result= SingleThreadCalculate(input);
+    //        sw.Start();
+    //        var result= SingleThreadCalculate(input);
 
-            Console.WriteLine(result);
+    //        Console.WriteLine(result);
 
-            sw.Stop();
+    //        sw.Stop();
 
-            Console.WriteLine(sw.ElapsedMilliseconds);
-        }
-
-
-        private long SingleThreadCalculate(List<long> slices)
-        {
-            var analyzer=new Analyzer();
-
-            var results = analyzer.Analyze(slices);
-
-            return results;
-        }
-
-        private long MultiThreadCaluate(List<long> input)
-        {
-            List<List<long>> slices = new List<List<long>>();
-
-            long sliceSize = 500;
-            for (long i = 0; i < input.Count; i += sliceSize)
-                slices.Add(input.Skip(i).Take(sliceSize).ToList());
-
-            var results = new List<long>();
+    //        Console.WriteLine(sw.ElapsedMilliseconds);
+    //    }
 
 
-            Parallel.ForEach(slices, (slice) =>
-            {
+    //    private long SingleThreadCalculate(List<long> slices)
+    //    {
+    //        var analyzer=new Analyzer();
 
-                var analyzer = new Analyzer();
-                long result = analyzer.Analyze(slice);  // هر Thread با داده خود کار می‌کند
-                lock (results) { results.Add(result); } // فقط collect نتیجه به تنظیم synchronization نیاز دارد
-            });
+    //        var results = analyzer.Analyze(slices);
 
-            return results.Sum();
-        }
-    }
+    //        return results;
+    //    }
+
+    //    private long MultiThreadCaluate(List<long> input)
+    //    {
+    //        List<List<long>> slices = new List<List<long>>();
+
+    //        long sliceSize = 500;
+    //        for (long i = 0; i < input.Count; i += sliceSize)
+    //            slices.Add(input.Skip(i).Take(sliceSize).ToList());
+
+    //        var results = new List<long>();
 
 
-    public class Analyzer
-    {
-        public long Analyze(List<long> data)
-        {
-            long sum = 0;
-            foreach (var num in data)
-                sum += num;
-            return sum; // هر Thread کپی sum خودش را دارد و فقط روی همان کار می‌کند
-        }
+    //        Parallel.ForEach(slices, (slice) =>
+    //        {
+
+    //            var analyzer = new Analyzer();
+    //            long result = analyzer.Analyze(slice);  // هر Thread با داده خود کار می‌کند
+    //            lock (results) { results.Add(result); } // فقط collect نتیجه به تنظیم synchronization نیاز دارد
+    //        });
+
+    //        return results.Sum();
+    //    }
+    //}
+
+
+    //public class Analyzer
+    //{
+    //    public long Analyze(List<long> data)
+    //    {
+    //        long sum = 0;
+    //        foreach (var num in data)
+    //            sum += num;
+    //        return sum; // هر Thread کپی sum خودش را دارد و فقط روی همان کار می‌کند
+    //    }
     }
 }
