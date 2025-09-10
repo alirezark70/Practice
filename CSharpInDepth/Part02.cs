de52b3ff8810257d9e5cdbf58f8987e2c98d3f51
alirezark70 <rezaee.alireza7098@gmail.com>
2025-09-10 19:10:28 +0330

    read c# in depth part2 modern envent

---
+++ CSharpInDepth/Part02.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CSharpInDepth
+{
+    internal class Part02
+    {
+    }
+
+
+    public class ModerEventExample
+    {
+        //تعریف یک ایونت هندلر با 
+        //nullable reference type
+        public event EventHandler<string>? _messageRecived;
+
+
+        private static void HandlerMessage(object? sender , string? message)
+        {
+            Console.WriteLine($"Message recived from {sender?.GetType().Name} : {message}");
+        }
+
+
+        private static void LogMessage(object? sender, string? message)
+        {
+            Console.WriteLine($"[LOG] {DateTime.Now} : {message} ");
+        }
+
+        public void Execute()
+        {
+            _messageRecived += HandlerMessage;
+            _messageRecived += LogMessage;
+
+            _messageRecived.Invoke(this, "Test Hanlder");
+
+
+        }
+
+    }
+
+    
+}
---
+++ CSharpInDepth/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using CSharpInDepth;
+
+ModerEventExample moderEvent=new ModerEventExample();
+
+moderEvent.Execute();
+
+
+Console.WriteLine("Hello, World!");
